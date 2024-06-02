8065a99f645b0ecf4ced00c4a87057bb52e05b2a
ajaiverma1986 <45514244+ajaiverma1986@users.noreply.github.com>
2024-06-03 00:06:22 +0530

    Add project files.

---
+++ SANYUKT.Commonlib/Security/CustomAuthorizationRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using SANYUKT.Datamodel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SANYUKT.Commonlib.Security
+{
+    public class CustomAuthorizationRequirement : IAuthorizationRequirement
+    {
+        public Permissions Permission { get; set; }
+    }
+}
